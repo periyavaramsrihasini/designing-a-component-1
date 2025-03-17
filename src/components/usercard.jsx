f4057129bee325528132d6baa914af1660663d4a
unknown <hasiniperiyavaram@gmail>
2025-03-17 14:57:13 +0530

    added files

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
 // write the component code here
+// write the component code here
+
+import React from 'react';
+import '../index.css'; 
+
+const UserCard = () => {
+
+  return (
+    <div className="user-card">
+      <img src='https://i.pinimg.com/736x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg' alt="Profile" className="photo" />
+      <div className="details">
+        <h1>#Kalvian UserCard</h1>
+        <h2 className="name">Hasini</h2>
+        <p className="email">hasiniperiyavaram@gmail.com</p>
+        <p className="phone">Phone: 1234567890</p>
+        <p className="address">Address:The Apollo University, Chittoor, Andhra Pradesh, India </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
